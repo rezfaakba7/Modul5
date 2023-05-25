2e1bb02fd99c8c7815e700a1e22055f1f11ce766
rezfaakbar <130022244+rezfaakba7@users.noreply.github.com>
2023-05-25 18:19:32 +0800

    Add files via upload

---
+++ BinaryTree.java
@@ -0,0 +1,66 @@
+package Latihan5;
+
+
+public class BinaryTree {
+    public Node root;
+
+    public void NewNode(int data) {
+        root = NewNode(root, new Node(data));
+    }
+
+    private Node NewNode(Node root, Node newData) {
+        if (root == null) {
+            root = newData;
+            return root;
+        }
+
+        if (newData.data < root.data) {
+            root.left = NewNode(root.left, newData);
+        }else{
+            root.right = NewNode(root.right, newData);
+        }
+        return root;
+    }
+
+    public void inOrder(Node node){
+        if(node != null){
+            inOrder(node.left);
+            System.out.println(node.data + " ");
+            inOrder(node.right);
+        }
+    }
+
+    public void preOrder(Node node){
+        if(node != null){
+            System.out.println(node.data + " ");
+            preOrder(node.left);
+            preOrder(node.right);
+        }
+    }
+
+    public void postOrder(Node node){
+        if(node != null){
+            postOrder(node.left);
+            postOrder(node.right);
+            System.out.println(node.data + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        BinaryTree pohon = new BinaryTree();
+
+        pohon.NewNode(20);
+        pohon.NewNode(2);
+        pohon.NewNode(25);
+        pohon.NewNode(37);
+        pohon.NewNode(16);
+
+        System.out.println("\nPre Order: ");
+        pohon.preOrder(pohon.root);
+        System.out.println("\nIn Order: ");
+        pohon.inOrder(pohon.root);
+        System.out.println("\nPost Order: ");
+        pohon.postOrder(pohon.root);
+    }
+}
+
